59565665efc96253c2f524fded8667f5d047d49c
A <alejandro@mosteo.com>
2018-02-27 22:37:22 +0100

    More deletions non-sqlite

--- delete_unused.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-[ "$1" != "" ] || { echo Give gpr file; exit 1; }
-
-{ 
-  ls *.ad{s,b}
-  cat $1 | grep -E '.*ad(s|b)' | sed 's/"//g; s/,//; s/(//; s/[[:space:]]//g' | sort -u 
-} | sort | uniq -u | parallel -j1 '[ -f {} ] && git rm {}'
