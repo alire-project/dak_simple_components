455d405030d252b6261e652c8fe7e58acc898c90
A <alejandro@mosteo.com>
2018-02-27 21:57:17 +0100

    Deleted base components tests

--- delete_used.sh
+++ delete_used.sh
@@ -2,4 +2,4 @@
 
 [ "$1" != "" ] || { echo Give gpr file; exit 1; }
 
-cat $1 | grep -E '.*ad(s|b)' | sed 's/"//g; s/,//; s/(//; s/[[:space:]]//g' | parallel -j1 '[ -f {} ] && git rm {}'
+cat $1 | grep -E '.*ad(s|b)' | sed 's/"//g; s/,//; s/(//; s/[[:space:]]//g' | sort -u | parallel -j1 '[ -f {} ] && git rm {}'
