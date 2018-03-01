c970a6aa8d9c94bacde0512506baae20d4622ab6
Alejandro R Mosteo <alejandro@mosteo.com>
2018-03-01 14:05:26 +0100

    Post-secure packaging

--- delete_used.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-[ "$1" != "" ] || { echo Give gpr file; exit 1; }
-
-cat $1 | grep -E '.*ad(s|b)' | sed 's/"//g; s/,//; s/(//; s/[[:space:]]//g' | sort -u | parallel -j1 '[ -f {} ] && git rm {}'
